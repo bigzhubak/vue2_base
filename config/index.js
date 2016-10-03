8d0415fcc61f945dda098b79f2ad1f3a5b30ed1f
bigzhu <vermiliondun@gmail.com>
2016-10-03 21:50:50 +0800

    dist 生成在执行的目录

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(process.cwd(), './dist/index.html'),
+    assetsRoot: path.resolve(process.cwd(), './dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
