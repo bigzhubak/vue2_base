5aa5c805bed70cc47b7bde3a6cbe03efd339340f
bigzhu <vermiliondun@gmail.com>
2016-11-03 10:16:45 +0800

    能放外部的不要打包进来

--- build/externals.js
+++ build/externals.js
@@ -2,6 +2,10 @@ module.exports = {
   jQuery: 'jQuery',
   jquery: 'jQuery',
   vue: 'Vue',
+  lodash: '_',
+  'whatwg-fetch': 'fetch',
+  toastr: 'toastr',
+  autolinker: 'Autolinker',
   'vuex': 'Vuex',
   'vue-router': 'VueRouter',
   'vue-resource': 'VueResource'
